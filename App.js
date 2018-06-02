863fd03f6b9c8e791f9b14c61999f4854168f78f
Unknown <pssoni@uwaterloo.ca>
2018-06-02 13:46:05 -0400

    adsd

--- App.js
+++ App.js
@@ -1,57 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-	Platform,
-	StyleSheet,
-	Text,
-	View, 
-	TextInput
-} from 'react-native';
+export default class App extends React.Component{
+  render(){
+    return <RootStack />;
+  }
+
+} 
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+         <Button
+            title="Wellness Tools"
+            onPress={() => this.props.navigation.navigate({this.props.title}!)
+          />
+          <Button
+            title="Wellness"
+            onPress={() => this.props.navigation.navigate('Profile')}
+          />
+      </View>
+    );
+  }
+}
+
+class MenuOption extends Component {
+  render() {
+    return (
+      <View style={styles.button1}>
+       <Text>Hello {this.props.name}!</Text>
+        <Button
+          onPress={doNothing}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
+}
+
+class WellnessTools extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+       <MenuOption name='WellnessTools' />
+      </View>
+    );
+  }
+}
+
+let RootStack = createStackNavigator(
+  {
+     Home: {
+      screen: HomeScreen,
+      },
+      WellnessTools:{
+        screen: WellnessTools
+      }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 
-const instructions = Platform.select({
-	ios: 'Press Cmd+R to reload,\n' +
-		'Cmd+D or shake for dev menu',
-	android: 'Double tap R on your keyboard to reload,\n' +
-		'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component < Props > {
-	render() {
-		return(
-			<View style={styles.container}>
-				<Text style={styles.welcome}>
-					Welcome to React Native!
-				</Text>
-				<TextInput onChangeText={(text) => this.setState({text})} >
-				
-				</TextInput>
-			</View>
-		);
-	}
+
+
+let doNothing = function (){
+  
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
+  button1: {
+    position: 'absolute',
+    bottom: 0,
+    left:0,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
