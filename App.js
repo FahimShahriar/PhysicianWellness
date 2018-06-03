ca8accce87780bec3fe30cc89ab5f4c79703a1e0
Unknown <pssoni@uwaterloo.ca>
2018-06-03 01:23:04 -0400

    sdsadsaasd

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 
 export default class App extends React.Component{
   render(){
     return <RootStack />;
+
   }
 
 } 
@@ -14,44 +15,64 @@ class HomeScreen extends Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
-         <Button
+         <TouchableOpacity 
+            style = {styles.button}
+            onPress={() => this.props.navigation.navigate('WellnessTools')}>
+            <Image source={require('/Users/prabhdeep/hello/AwesomeProject/Picture1.png')}
+              style={{width: 100, height: 100}} />
+          </TouchableOpacity>
+          <Button
+            title="Wellness"
+            onPress={() => this.props.navigation.navigate('Wellness')}
+          />
+          <Button
             title="Wellness Tools"
-            onPress={() => this.props.navigation.navigate({this.props.title}!)
+            onPress={() => this.props.navigation.navigate('WellnessTools')}
           />
           <Button
             title="Wellness"
             onPress={() => this.props.navigation.navigate('Profile')}
           />
-      </View>
+        </View>
     );
   }
 }
+
+
 
 class MenuOption extends Component {
   render() {
     return (
-      <View style={styles.button1}>
-       <Text>Hello {this.props.name}!</Text>
-        <Button
-          onPress={doNothing}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+        style = {styles.text}
       </View>
     );
   }
 }
 
-class WellnessTools extends Component {
+
+class WellnessTools extends MenuOption {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-       <MenuOption name='WellnessTools' />
+      <View>
+       <MenuOption name=' ' />
       </View>
     );
   }
 }
+
+
+class Wellness extends MenuOption {
+  render() {
+    return (
+      <View>
+       <MenuOption name='Wellness' />
+      </View>
+    );
+  }
+}
+
 
 let RootStack = createStackNavigator(
   {
@@ -60,6 +81,9 @@ let RootStack = createStackNavigator(
       },
       WellnessTools:{
         screen: WellnessTools
+      },
+      Wellness:{
+        screen: Wellness
       }
   },
   {
@@ -71,17 +95,20 @@ let RootStack = createStackNavigator(
 
 
 
-let doNothing = function (){
+
   
-}
 
 const styles = StyleSheet.create({
-  button1: {
+  text: {
     position: 'absolute',
-    bottom: 0,
     left:0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    padding: 10,
+    backgroundColor: '#DDDDDD',
+    alignItems: 'center',
+  }
 });
