06887f56c9954ebb11d4a48037d55241c83a18dd
Prabhdeep <pssoni@uwaterloo.ca>
2018-06-19 22:54:33 -0400

    3

--- components/email.js
+++ components/email.js
@@ -4,6 +4,7 @@ import { Text, View, TouchableOpacity, ImageBackground, Image, Picker} from 'rea
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 import styles from './styles.js'
 import { Dropdown } from 'react-native-material-dropdown';
+import RF from "react-native-responsive-fontsize";
 
 export default class Email extends Component {
     render() {
@@ -18,25 +19,21 @@ export default class Email extends Component {
           }];
        
       return (
-        <View style = {styles.fill}>
+        <View style = {{flex:1, padding: '3%'}}>
 
 
 
         <Dropdown
-        label='Favorite Fruit'
-        data={data}
-        dropdownPosition = {0}
+          label='Send an email to: '
+          data={data}
+          dropdownPosition = {0}
+          labelFontSize = {RF(2)}
+          fontSize = {RF(2)}
       />
-        {/*<FormLabel>Name</FormLabel>
+        <FormLabel style = {styles.fill}>Name</FormLabel>
             <FormInput/>
 
-        <Picker
-            selectedValue={'java'}
-            style={{ height: 50, width: 100 }}
-            onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
-            </Picker>*/}
+        
             </View>
       )
     }
