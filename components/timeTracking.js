2a8617862270cc873ff3dc70dfbb34dd2d030cf0
Prabhdeep <pssoni@uwaterloo.ca>
2018-06-21 18:44:56 -0400

    got the stupid enter button to work

--- components/timeTracking.js
+++ components/timeTracking.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, TouchableOpacity, ImageBackground, StyleSheet, Image, Linking, ScrollView, Picker, TextInput} from 'react-native';
+import { Text, View, TouchableOpacity, ImageBackground, StyleSheet, Image, Linking, ScrollView, Picker, TextInput, Button} from 'react-native';
 
 import Icon from '../components/icon.js';
 import AskExpert from './AskExpert';
@@ -15,15 +15,14 @@ export default class TimeTracking extends Component {
     constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['Head','Head2'],
+      tableHead: ['Activity','Time Spent'],
       tableData: [
-        ['1', '2'],
-        ['a', 'b'],
-        ['1', '2'],
-        [' ', ' ']
+       
       ], 
-      activity: '',
-      time: ''
+      activity: 'Enter an activity',
+      time: 'Enter time spent',
+      total: 111
+
     }
     
   }
@@ -38,6 +37,7 @@ export default class TimeTracking extends Component {
       tableData[3] = {...tableData[3], key: ' jbhg'};
     this.setState({ tableData });*/}
 
+    this.setState({activity: text})
     const tableData1 = [];
     const rowData = [];
       for (let j = 0; j < 2; j += 1) {
@@ -46,37 +46,76 @@ export default class TimeTracking extends Component {
       tableData1.push(rowData);
 
       this.setState({tableData: tableData1})
+      
     ;
     }
 
+    let total = () => {
+      let sum=0;
+      for(let i=0; i< this.state.tableData.length; i+=1){
+        sum += this.state.tableData[i][1];
+        console.log(this.state.tableData[i][1])
+      }
+
+      this.setState({total: sum})
+
+      total();
+     
+      
+    }
     return (
       <ScrollView style = {styles.fill}>
       <View style={styles1.container}>
         <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
           <Row data={state.tableHead} style={styles1.head} textStyle={styles1.text}/>
           <Rows data={this.state.tableData} textStyle={styles1.text}/>
-          
+       
         </Table>
         <TextInput 
           style = {styles.fill} 
-          onChangeText={
-            (text) => {this.setState({activity: text})
-            update()}
+          editable = {true}
+          value = {this.state.activity}
+          onSubmitEditing ={ 
+            
+            (event) => {
+            let text = event.nativeEvent.text;
+           
+
+            
+               this.setState({activity: text})
+               const tableData1 = [];
+          const rowData = [];
+    for (let j = 0; j < 2; j += 1) {
+      rowData.push(this.state.activity);
+    }
+    tableData1.push(rowData);
+
+    this.setState({tableData: tableData1}) 
+    total()}
+    
+
           }
+          
+          onChangeText ={(text) => update (text) }
         /> 
+
+        <Text> {this.state.total}</Text>
         
 
         {/*    <TextInput style = {styles.fill} onChangeText={() => this.setState({tableData: tableData1})}/>*/}
         <Text> {this.state.activity} </Text>
+   
+            
+            
+            title = {'submit'}/>
       </View>
       </ScrollView>
     )
   }
 }
 
-let update = function () {
-  
-}
+
+
  
 const styles1 = StyleSheet.create({
   container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
