5a5b12eeb8af116ed2cc6fad46144cf55f467780
Prabhdeep <pssoni@uwaterloo.ca>
2018-06-22 15:54:45 -0400

    got the total and entry both working {1}

--- components/timeTracking.js
+++ components/timeTracking.js
@@ -16,12 +16,11 @@ export default class TimeTracking extends Component {
     super(props);
     this.state = {
       tableHead: ['Activity','Time Spent'],
-      tableData: [
-       
-      ], 
+      tableData: [], 
+      tableData2: [], 
       activity: 'Enter an activity',
       time: 'Enter time spent',
-      total: 111
+      total: ''
 
     }
     
@@ -31,83 +30,100 @@ export default class TimeTracking extends Component {
   render() {
     const state = this.state;
 
-
-    let update = (text) => {
-    {/*}  let tableData = [ state.tableData ];
-      tableData[3] = {...tableData[3], key: ' jbhg'};
-    this.setState({ tableData });*/}
-
-    this.setState({activity: text})
-    const tableData1 = [];
-    const rowData = [];
-      for (let j = 0; j < 2; j += 1) {
-        rowData.push(this.state.activity);
-      }
-      tableData1.push(rowData);
-
-      this.setState({tableData: tableData1})
-      
-    ;
+    let setter = (text) => {
+      this.setState({activity: text}, () => {activityTot()})
+     
     }
+
+    
+
+    let activityTot = () => {
+      const rowData = [];
+
+  
+    
+      rowData.push(this.state.activity)
+      this.setState({tableData2: rowData})
+    }
+
+    let setter2 = (text) => {
+      this.setState({time: text}, () => setter3())
+    }
+
+    let setter3 = (text) => {
+
+      
+      const tableData1 = this.state.tableData;
+      const rowData = this.state.tableData2;
+
+      rowData.push(this.state.time);
+
+    
+      tableData1.push(rowData);
+  
+      this.setState({tableData: tableData1})
+
+      const empty = [];
+      this.setState({tableData2: empty}, () => {total()} )
+      }
+
 
     let total = () => {
       let sum=0;
-      for(let i=0; i< this.state.tableData.length; i+=1){
-        sum += this.state.tableData[i][1];
-        console.log(this.state.tableData[i][1])
+      for(let i=0; i<this.state.tableData.length; i++){
+        sum += parseInt(this.state.tableData[i][1], 10)
       }
 
-      this.setState({total: sum})
+      console.log(sum)
+      this.setState({total: sum}), () => {}
+      }
 
 
-     
-      
-    }
     return (
       <ScrollView style = {styles.fill}>
       <View style={styles1.container}>
         <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
           <Row data={state.tableHead} style={styles1.head} textStyle={styles1.text}/>
           <Rows data={this.state.tableData} textStyle={styles1.text}/>
-       
         </Table>
         <TextInput 
           style = {styles.fill} 
           editable = {true}
           value = {this.state.activity}
           onSubmitEditing ={ 
-            
             (event) => {
+              let text = event.nativeEvent.text;
+              setter(text)
+              activityTot();
+             }
+          }
+        
+         /> 
+
+           <TextInput 
+          style = {styles.fill} 
+          editable = {true}
+          value = {this.state.time}
+          onSubmitEditing ={ (event) => {
             let text = event.nativeEvent.text;
+            setter2(text)
            
-
-            
-               this.setState({activity: text})
-               const tableData1 = [];
-          const rowData = [];
-    for (let j = 0; j < 2; j += 1) {
-      rowData.push(this.state.activity);
-    }
-    tableData1.push(rowData);
-
-    this.setState({tableData: tableData1}) 
-    total()}
+        }
     
 
           }
           
-          onChangeText ={(text) => update (text) }
+       
         /> 
 
-        <Text> {this.state.total}</Text>
+        <Text> {this.state.activity}</Text>
         
 
         {/*    <TextInput style = {styles.fill} onChangeText={() => this.setState({tableData: tableData1})}/>*/}
-        <Text> {this.state.activity} </Text>
+        <Text> {this.state.time} </Text>
+        <Text> {this.state.total} </Text>
    
-            
-            
-            title = {'submit'}/>
+=
       </View>
       </ScrollView>
     )
