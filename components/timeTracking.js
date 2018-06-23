d90d9091d23921d549dd5b0664c957c983f47c27
Prabhdeep <pssoni@uwaterloo.ca>
2018-06-22 21:20:01 -0400

    got placeholder text in

--- components/timeTracking.js
+++ components/timeTracking.js
@@ -18,9 +18,11 @@ export default class TimeTracking extends Component {
       tableHead: ['Activity','Time Spent'],
       tableData: [], 
       tableData2: [], 
-      activity: 'Enter an activity',
-      time: 'Enter time spent',
-      total: ''
+      activity: '',
+      time: '',
+      total: '',
+      tableFoot: []
+
 
     }
     
@@ -30,52 +32,41 @@ export default class TimeTracking extends Component {
   render() {
     const state = this.state;
 
-    let setter = (text) => {
-      this.setState({activity: text}, () => {activityTot()})
-     
+    let setActivity = (text) => {
+      this.setState( 
+        {activity: text}, 
+        () => { 
+          const rowData = [];
+          rowData.push(this.state.activity)
+          this.setState({tableData2: rowData})
+        }
+      )
     }
 
-    
-
-    let activityTot = () => {
-      const rowData = [];
-
-  
-    
-      rowData.push(this.state.activity)
-      this.setState({tableData2: rowData})
+    let setTime = (text) => {
+      this.setState(
+        {time: text}, 
+        () => { 
+          const tableData1 = this.state.tableData;
+          const rowData = this.state.tableData2;
+          rowData.push(this.state.time);
+          tableData1.push(rowData);
+          this.setState({tableData: tableData1})
+          const empty = [];
+          this.setState(
+            {tableData2: empty}, 
+            () => { total() } 
+          )
+        }
+      )
     }
-
-    let setter2 = (text) => {
-      this.setState({time: text}, () => setter3())
-    }
-
-    let setter3 = (text) => {
-
-      
-      const tableData1 = this.state.tableData;
-      const rowData = this.state.tableData2;
-
-      rowData.push(this.state.time);
-
-    
-      tableData1.push(rowData);
-  
-      this.setState({tableData: tableData1})
-
-      const empty = [];
-      this.setState({tableData2: empty}, () => {total()} )
-      }
-
 
     let total = () => {
       let sum=0;
-      for(let i=0; i<this.state.tableData.length; i++){
+      for(let i=0; i<this.state.tableData.length; i++) {
         sum += parseInt(this.state.tableData[i][1], 10)
       }
-
-      console.log(sum)
-      this.setState({total: sum}), () => {}
+      this.setState({total: sum}, () => {})
       }
 
 
@@ -86,42 +77,46 @@ export default class TimeTracking extends Component {
           <Row data={state.tableHead} style={styles1.head} textStyle={styles1.text}/>
           <Rows data={this.state.tableData} textStyle={styles1.text}/>
         </Table>
-        <TextInput 
-          style = {styles.fill} 
-          editable = {true}
-          value = {this.state.activity}
-          onSubmitEditing ={ 
-            (event) => {
-              let text = event.nativeEvent.text;
-              setter(text)
-              activityTot();
-             }
-          }
-        
-         /> 
 
-           <TextInput 
-          style = {styles.fill} 
-          editable = {true}
-          value = {this.state.time}
-          onSubmitEditing ={ (event) => {
-            let text = event.nativeEvent.text;
-            setter2(text)
-           
-        }
-    
-
-          }
-          
+        <View style = {styles.row}>
+          <View style = {styles1.container2}>
+            <TextInput 
+              style = {styles.fill} 
        
-        /> 
+             
+              value = {this.state.activity}
+              placeholder = 'Enter an activity'
+              editable = {true}
+              onSubmitEditing ={ 
+                (event) => {
+                  let text = event.nativeEvent.text;
+                  setActivity(text)
+                }
+              }
+            />
+          </View>
+          <View style = {styles1.container2}>
+            <TextInput 
+              style = {styles.container2} 
+              editable = {true}
+              placeholder = 'Enter a time in minutes'
+              value = {this.state.time}
+              onSubmitEditing ={ 
+                (event) => {
+                  let text = event.nativeEvent.text;
+                  setTime(text)    
+                }
+              }
+            /> 
+          </View>
+        </View>
 
         <Text> {this.state.activity}</Text>
         
 
         {/*    <TextInput style = {styles.fill} onChangeText={() => this.setState({tableData: tableData1})}/>*/}
         <Text> {this.state.time} </Text>
-        <Text> {this.state.total} </Text>
+        <Text> Total: {this.state.total} </Text>
    
 =
       </View>
@@ -134,7 +129,10 @@ export default class TimeTracking extends Component {
 
  
 const styles1 = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff',  borderColor: 'rgba(0,0,0,1)',
+  borderBottomWidth: 1, },
+  container2: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff',  borderColor: '#000000',
+  borderWidth: 1,  },
   head: { height: 40, backgroundColor: '#f1f8ff' },
   text: { margin: 6 }
 });
