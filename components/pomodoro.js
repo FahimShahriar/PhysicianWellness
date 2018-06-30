d481fadfa150813f3255166ab57ead613eed7207
Prabhdeep <pssoni@uwaterloo.ca>
2018-06-29 22:57:52 -0400

    Pomodoro text added

--- components/pomodoro.js
+++ components/pomodoro.js
@@ -13,29 +13,15 @@ import styles from './styles.js'
 
 
 export default class Pomodoro extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['Activity','Time Spent'],
-      tableData: [], 
-      tableData2: [], 
-      activity: '',
-      time: '6',
-      total: '',
-      tableFoot: [],
-      filled: false,
-      timer: 2,
-      yes: true
-
-
-    }
-    
-  }
- 
-  
   render() {
     return (
-      <View style = {{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+      <ImageBackground 
+      style = { styles.fill }
+      source = { require('../Pictures/Backgrounds/pomodoro_screen.png' )}
+      imageStyle = {styles.background}
+    >
+      <View style = {styles.column}>
+      <View style = {styles.rowTop}>
         <CountDown
         onPress={() => alert('hello')}
         until = {1500}
@@ -43,77 +29,22 @@ export default class Pomodoro extends Component {
         size = {35}
         style = {styles.fill}
       />
+      </View>
+      <View style = {styles.description}>
+      <Text style = {styles.descriptionText}>
+      The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. 
+      The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, 
+      separated by short breaks. The technique has been widely popularized by dozens of apps and websites 
+      providing timers and instructions. Closely related to concepts such as timeboxing and iterative and 
+      incremental development used in software design, the method has been adopted in pair programming 
+      contexts.
+
+      </Text>
+      </View>
+ 
     </View>
  
+      </ImageBackground>
     )
   }
 }
-
-class Setter extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['Activity','Time Spent'],
-      tableData: [], 
-      tableData2: [], 
-      activity: '',
-      time: '',
-      total: '',
-      tableFoot: [],
-      filled: false,
-      timer: 2,
-      yes: true
-
-
-    }
-    
-  }
-  render() {
-
-    let setTime = () => {
-      this.setState({timer: 5}, () => {})
-      console.log(this.state.timer)
-      
-    }
-  return(
-    <CountDown
-        until={this.state.timer}
-       
-        onPress={() => alert('hello')}
-        size={20}
-        style = {styles.fill}
-        onFinish = {() => {this.setState({timer: 5}, () => {console.log(this.state.timer)})}
-          }
-      />
-  )
-  }
-}
-
-class Setter1 extends Component{
-  render() {
-  return(
-    <CountDown
-        until={3}
-       
-        onPress={() => alert('hello')}
-        size={20}
-        style = {styles.fill}
-        onFinish = {() => {}}
-      />
-  )
-  }
-}
-
-
-
- 
-const styles1 = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff',  borderColor: 'rgba(0,0,0,1)',
-  borderBottomWidth: 1, },
-  container2: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff',  borderColor: '#000000',
-  borderWidth: 1,  },
-  head: { height: 40, backgroundColor: '#f1f8ff' },
-  text: { margin: 6 }
-});
-
-
